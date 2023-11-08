8bb50965a14f12985e4ec31de7395a6780c5f05c
Abhishek Thakur <126442789+thakur1804@users.noreply.github.com>
2023-11-08 17:33:57 +0530

    ADDED: background animation prototype

--- src/assets/styles/global.ts
+++ src/assets/styles/global.ts
@@ -1,11 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100&display=swap');
+
     *, *::before, *::after{
         margin: 0;
         padding: 0;
         outline:0;
         box-sizing:border-box;
+        font-family: 'Poppins', sans-serif;
         font-size: 62.5%;
         font-synthesis: none;
         color-scheme: dark;
